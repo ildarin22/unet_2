0cf0434470ba70715929cac5f6d3b7c63b641655
ildarin <ildarin24@yandex.ru>
2019-01-21 16:46:11 +0300

    office update

---
+++ data_load.py
@@ -0,0 +1,42 @@
+import numpy as np
+import os
+
+from keras_preprocessing.image import load_img, img_to_array
+from tqdm import tqdm_notebook
+from skimage.transform import resize
+
+# Set some parameters
+im_width = 128
+im_height = 128
+border = 5
+
+
+
+# Get and resize train images and masks
+def get_data(path, train=True):
+    ids = next(os.walk(path + "images"))[2]
+    X = np.zeros((len(ids), im_height, im_width, 1), dtype=np.float32)
+    if train:
+        y = np.zeros((len(ids), im_height, im_width, 1), dtype=np.float32)
+    print('Getting and resizing images ... ')
+    for n, id_ in tqdm_notebook(enumerate(ids), total=len(ids)):
+        # Load images
+        img = load_img(path + '/images/' + id_, color_mode = "grayscale")
+        x_img = img_to_array(img)
+        x_img = resize(x_img, (128, 128, 1), mode='constant', preserve_range=True)
+
+        # Load masks
+        if train:
+            mask = img_to_array(load_img(path + '/masks/' + id_, color_mode = "grayscale"))
+            mask = resize(mask, (128, 128, 1), mode='constant', preserve_range=True)
+
+        # Save images
+        X[n, ..., 0] = x_img.squeeze() / 255
+        if train:
+            y[n] = mask / 255
+    print('Done!')
+    if train:
+        return X, y
+    else:
+        return X
+
---
+++ plot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use("ggplot")
+
+def plotting(results):
+    plt.figure(figsize=(8, 8))
+    plt.title("Learning curve")
+    plt.plot(results.history["loss"], label="loss")
+    plt.plot(results.history["val_loss"], label="val_loss")
+    plt.plot(np.argmin(results.history["val_loss"]), np.min(results.history["val_loss"]), marker="x", color="r", label="best model")
+    plt.xlabel("Epochs")
+    plt.ylabel("log_loss")
+    plt.legend()
+    plt.show()

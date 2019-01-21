73f3c5a463bb483097e4ea9316ab8e4564c025d9
ildar <ildarin24@yandex.ru>
2019-01-22 01:45:52 +0300

    add unet_v2

--- data_load.py
+++ data_load.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
-
+import skimage.io as io
+import skimage.transform as trans
 from keras_preprocessing.image import load_img, img_to_array
 from tqdm import tqdm_notebook
 from skimage.transform import resize
@@ -10,7 +11,21 @@ im_width = 128
 im_height = 128
 border = 5
 
+Sky = [128,128,128]
+Building = [128,0,0]
+Pole = [192,192,128]
+Road = [128,64,128]
+Pavement = [60,40,222]
+Tree = [128,128,0]
+SignSymbol = [192,128,128]
+Fence = [64,64,128]
+Car = [64,0,128]
+Pedestrian = [64,64,0]
+Bicyclist = [0,128,192]
+Unlabelled = [0,0,0]
 
+COLOR_DICT = np.array([Sky, Building, Pole, Road, Pavement,
+                          Tree, SignSymbol, Fence, Car, Pedestrian, Bicyclist, Unlabelled])
 
 # Get and resize train images and masks
 def get_data(path, train=True):
@@ -41,3 +56,27 @@ def get_data(path, train=True):
     else:
         return X
 
+
+
+def testGenerator(test_path,num_image,target_size = (128,128),flag_multi_class = False,as_gray = True):
+    for i in range(num_image):
+        img = io.imread(os.path.join(test_path,"%d.png"%i),as_gray = as_gray)
+        img = img / 255
+        img = trans.resize(img,target_size)
+        img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
+        img = np.reshape(img,(1,)+img.shape)
+        yield img
+
+
+def labelVisualize(num_class,color_dict,img):
+    img = img[:,:,0] if len(img.shape) == 3 else img
+    img_out = np.zeros(img.shape + (3,))
+    for i in range(num_class):
+        img_out[img == i,:] = color_dict[i]
+    return img_out / 255
+
+
+def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
+    for i,item in enumerate(npyfile):
+        img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
+        io.imsave(os.path.join(save_path,"%d_predict.png"%i),img)
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
-from data_load import get_data
+from data_load import get_data, testGenerator, saveResult
 import model
 import numpy as np
 from keras.layers import Input
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from plot import plotting, plot_sample
-from keras.optimizers import Adam
+
 
 # %matplotlib inline
 
@@ -22,38 +22,41 @@ X, y = get_data(path_train, train=True)
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.15, random_state=2018)
 
 
-def train():
+def train(ver_2 = False):
     callbacks = [
         EarlyStopping(patience=10, verbose=1),
-        ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1),
+        # ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1),
         ModelCheckpoint('model-bottles.h5', verbose=1, save_best_only=True, save_weights_only=True)
     ]
 
-    input_img = Input((im_height, im_width, 1), name='img')
-    unet = model.get_unet( input_img, n_filters=16, dropout=0.05, batchnorm=True)
-    unet.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=["accuracy"])
-    # model.summary()
+    if(ver_2):
+        unet = model.get_unet_v2()
+    else:
+        unet = model.get_unet_v1(n_filters=16, dropout=0.05, batchnorm=True)
+
     results = unet.fit(X_train, y_train, batch_size=8, epochs=100, callbacks=callbacks,
                     validation_data=(X_valid, y_valid))
-
     plotting(results)
-    return unet
 
-def predict():
-    input_img = Input((im_height, im_width, 1), name='img')
-    unet = model.get_unet(input_img, n_filters=16, dropout=0.05, batchnorm=True)
-    unet.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=["accuracy"])
+def predict(ver_2 = False):
+    if (ver_2):
+        unet = model.get_unet_v2()
+    else:
+        unet = model.get_unet_v1(n_filters=16, dropout=0.05, batchnorm=True)
+
     unet.load_weights('model-bottles.h5')
     # unet.evaluate(X_valid, y_valid, verbose=1)
     preds_train = unet.predict(X_train, verbose=1)
     preds_val = unet.predict(X_valid, verbose=1)
 
-    # Threshold predictions
     preds_train_t = (preds_train > 0.5).astype(np.uint8)
     preds_val_t = (preds_val > 0.5).astype(np.uint8)
     # plot_sample(X_train, y_train, preds_train, preds_train_t, ix=None)
-    plot_sample(X_valid, y_valid, preds_val, preds_val_t, ix=None)
+    # plot_sample(X_valid, y_valid, preds_val, preds_val_t, ix=None)
 
+    testGene = testGenerator(path_test,1)
+    results = unet.predict_generator(testGene, 1, verbose=1)
+    saveResult(path_test, results)
+# train(ver_2=True)
+predict(ver_2=True)
 
-# train()
-predict()
